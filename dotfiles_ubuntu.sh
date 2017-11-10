ac06c7d6e8bb467ffadf5eb2670396e9c23f6aef
Saito Naoya@MBP <saito3110.naoya@gmail.com>
2017-11-11 00:00:13 +0900

    create my macos setup

---
+++ dotfiles_ubuntu.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+while :; do
+    read -p "overwrite? (y/n):  " isOver
+    if [ "${isOver}" = "y" ]; then
+        ln -fs ~/dotfiles/vimrc ~/.vimrc
+        ln -fs ~/dotfiles/dein.toml ~/.dein.toml
+        ln -fs ~/dotfiles/sublime//Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User
+        ln -fs ~/dotfiles/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User
+        break
+    elif [ "${isOver}" = "n" ]; then
+        ln -s ~/dotfiles/vimrc ~/.vimrc
+        ln -s ~/dotfiles/dein.toml ~/.dein.toml
+        ln -s ~/dotfiles/sublime/Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User
+        ln -s ~/dotfiles/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User
+        break
+    else
+        echo "Please input y or n"
+    fi
+done
+
+
