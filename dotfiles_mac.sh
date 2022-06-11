c2dd5e1cb3c8b2ce0ee6b26b58f7ab2745b132b3
Saito Naoya <naoya@SaitonoMacBook-Pro.local>
2022-06-11 21:24:12 +0900

    バグ修正

--- dotfiles_mac.sh
+++ dotfiles_mac.sh
@@ -9,36 +9,37 @@ brew bundle --file ./macos-setup/Brewfile
 echo "========================================================================="
 echo "====== Finish : Homebrew-bundle =========================================="
 echo "========================================================================="
-
+echo ""
 echo "========================================================================="
 echo "====== Start : Shell setup =============================================="
 echo "========================================================================="
 
-echo "start : install neovim"
-pip install neovim
-echo "finish : install neovim"
-
-echo "start : chenge default shell bash -> zsh"
+echo "=== start : install neovim"
+pip3 install neovim
+echo "=== finish : install neovim"
+echo ""
+echo "=== start : chenge default shell bash -> zsh"
 sudo chsh -s /bin/zsh
-echo "finish : chenge default shell bash -> zsh"
-
-echo "start : clone 'prezto'"
-git clone --depth 1 https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-echo "finish : clone 'prezto'"
-
-echo "start : create symlinks"
+echo "=== finish : chenge default shell bash -> zsh"
+echo ""
+echo "=== start : clone 'prezto'"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+echo "=== finish : clone 'prezto'"
+echo ""
+echo "=== start : create symlinks"
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # ln -fs ./settingfile/vimrc ~/.vimrc
 # ln -fs ./settingfile/dein.toml ~/.dein.toml
 # ln -fs ./settingfile/latexmkrc ~/.latexmkrc
-ln -fs ./settingfile/nvim ~/.config/nvim
-ln -fs ~/.zprezto/runcoms/zlogin ~/.zlogin
-ln -fs ~/.zprezto/runcoms/zlogout ~/.zlogout
-ln -fs ~/.zprezto/runcoms/zprofile ~/.zprofile
-ln -fs ~/.zprezto/runcoms/zshenv ~/.zshenv
-ln -fs ./settingfile/zpreztorc ~/.zpreztorc
-ln -fs ./settingfile/zshrc ~/.zshrc
-echo "finish : create symlinks"
-
+ln -fsv ./settingfile/nvim ~/.config/nvim
+ln -fsv ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -fsv ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -fsv ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -fsv ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -fsv ${SCRIPT_DIR}/settingfile/zpreztorc ~/.zpreztorc
+ln -fsv ${SCRIPT_DIR}/settingfile/zshrc ~/.zshrc
+echo "=== finish : create symlinks"
+echo ""
 echo "========================================================================="
 echo "====== Finish : Shell setup ============================================="
 echo "========================================================================="
