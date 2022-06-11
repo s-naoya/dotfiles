2db5f496794e1648f079679ae7f0d5f44bf9edc0
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2022-06-11 12:26:13 +0900

    brew-bundle+Shellに変更

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dotfiles
+dotfiles
+===
+## macos
+1. [Homebrew](https://brew.sh/index_ja)をインストール
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
+2. AppStoreにログイン
+3. シェルスクリプトを実行
+```sh
+./dotfiles_mac.sh
+```
+4. 再起動
+5. 各アプリのセットアップを行う
+- Dockの設定を変更する
+- iTerm2の設定フォルダをsettingfileに変更
--- dotfiles_mac.sh
+++ dotfiles_mac.sh
@@ -1,22 +1,44 @@
 #! /bin/sh
 
-while :; do
-    read -p "overwrite dotfiles? (y/n): " isOver
-    if [ "${isOver}" = "y" ]; then
-        ln -fs ~/dotfiles/vimrc ~/.vimrc
-        ln -fs ~/dotfiles/dein.toml ~/.dein.toml
-        # ln -fs ~/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-        # ln -fs ~/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-        break
-    elif [ "${isOver}" = "n" ]; then
-        ln -s ~/dotfiles/vimrc ~/.vimrc
-        ln -s ~/dotfiles/dein.toml ~/.dein.toml
-        # ln -s ~/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-        # ln -s ~/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-        break
-    else
-        echo "Please input y or n"
-    fi
-done
+echo "========================================================================="
+echo "====== Start : Homebrew-bundle =========================================="
+echo "========================================================================="
 
+brew bundle --file ./macos-setup/Brewfile
 
+echo "========================================================================="
+echo "====== Finish : Homebrew-bundle =========================================="
+echo "========================================================================="
+
+echo "========================================================================="
+echo "====== Start : Shell setup =============================================="
+echo "========================================================================="
+
+echo "start : install neovim"
+pip install neovim
+echo "finish : install neovim"
+
+echo "start : chenge default shell bash -> zsh"
+sudo chsh -s /bin/zsh
+echo "finish : chenge default shell bash -> zsh"
+
+echo "start : clone 'prezto'"
+git clone --depth 1 https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+echo "finish : clone 'prezto'"
+
+echo "start : create symlinks"
+# ln -fs ./settingfile/vimrc ~/.vimrc
+# ln -fs ./settingfile/dein.toml ~/.dein.toml
+# ln -fs ./settingfile/latexmkrc ~/.latexmkrc
+ln -fs ./settingfile/nvim ~/.config/nvim
+ln -fs ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -fs ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -fs ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -fs ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -fs ./settingfile/zpreztorc ~/.zpreztorc
+ln -fs ./settingfile/zshrc ~/.zshrc
+echo "finish : create symlinks"
+
+echo "========================================================================="
+echo "====== Finish : Shell setup ============================================="
+echo "========================================================================="
